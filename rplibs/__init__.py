9521b524a5c65946f585efdf8ee92aee34e54952
Joan Hérisson <joan.herisson@univ-evry.fr>
2022-04-28 10:03:08 +0200

    chore: add additional files

--- rplibs/__init__.py
+++ rplibs/__init__.py
@@ -7,7 +7,7 @@ Created on Sep 09 2020
 from rplibs.rpPathway import rpPathway
 from rplibs.rpSBML import rpSBML
 from rplibs.rpGraph import rpGraph
-# from rplibs.inchikeyMIRIAM import inchikeyMIRIAM
+from rplibs.inchikeyMIRIAM import inchikeyMIRIAM
 from rplibs.rpReaction import rpReaction
 from rplibs.rpCompound import rpCompound
 # __all__ = ['rpSBML', 'rpGraph', 'inchikeyMIRIAM']
