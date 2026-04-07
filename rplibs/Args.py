230dc7dcf7a565602c0919484e44c2300cdcef83
Joan Hérisson <joan.herisson@univ-evry.fr>
2026-04-07 17:39:02 +0200

    chore(rplibs/Args.py): make generic build_args_parser()

--- rplibs/Args.py
+++ rplibs/Args.py
@@ -4,12 +4,14 @@ from brs_utils import add_logger_args
 
 
 def build_args_parser(
-    prog: str, description: str = "", epilog: str = ""
+    prog: str, description: str = "", epilog: str = "", m_add_args=None
 ) -> ArgumentParser:
 
     parser = ArgumentParser(prog=prog, description=description, epilog=epilog)
 
     # Build Parser with rptools common arguments
+    if m_add_args:
+        parser = m_add_args(parser)
     parser = add_arguments(parser)
 
     return parser
@@ -19,13 +21,13 @@ def add_arguments(parser: ArgumentParser) -> ArgumentParser:
     # Add arguments related to the logger
     parser = add_logger_args(parser)
 
-    parser.add_argument(
-        "--pathways",
-        required=True,
-        type=str,
-        nargs="+",
-        help="Pathways (rpSBML) to make statistics on",
-    )
+    # parser.add_argument(
+    #     "--pathways",
+    #     required=True,
+    #     type=str,
+    #     nargs="+",
+    #     help="Pathways (rpSBML) to make statistics on",
+    # )
 
     parser.add_argument(
         "--version",
